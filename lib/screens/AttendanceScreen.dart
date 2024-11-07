39d3c1b4e88e46af71bacb4aeb77eb3f3edb6a01
Shubham Dev <shubhamdev@192.168.1.3>
2024-11-07 11:06:09 +0530

    07Nov24

--- lib/screens/AttendanceScreen.dart
+++ lib/screens/AttendanceScreen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-class AttendanceScreen extends StatelessWidget {
+import 'package:smart_roll_call_flutter/models/student.dart';
+class AttendanceScreen extends StatefulWidget {
+  @override
+  _AttendanceScreenState createState() => _AttendanceScreenState();
+}
+
+class _AttendanceScreenState extends State<AttendanceScreen> {
   final List<Student> students = [
     Student(name: 'Shubham Dev', rollNumber: '001'),
     Student(name: 'Karan Bhatia', rollNumber: '002'),
@@ -14,19 +20,64 @@ class AttendanceScreen extends StatelessWidget {
     Student(name: 'Shashikant Sharma', rollNumber: '011'),
   ];
 
+  int presentCount = 0;
+
+  void _updateAttendance(bool isChecked) {
+    setState(() {
+      presentCount = students.where((student) => student.isPresent).length;
+    });
+  }
+
+  void _clearSelection() {
+    setState(() {
+      for (var student in students) {
+        student.isPresent = false;
+      }
+      presentCount = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Attendance'),
+        actions: [
+          TextButton(
+            onPressed: _clearSelection,
+            child: Text(
+              'Clear',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total Students: ${students.length}',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Text(
+                  'Present: $presentCount',
+                  style: TextStyle(fontSize: 16, color: Colors.green),
+                ),
+              ],
+            ),
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: students.length,
               itemBuilder: (context, index) {
-                return StudentCard(student: students[index]);
+                return StudentCard(
+                  student: students[index],
+                  onChanged: _updateAttendance,
+                );
               },
             ),
           ),
@@ -47,16 +98,15 @@ class AttendanceScreen extends StatelessWidget {
 
 class StudentCard extends StatefulWidget {
   final Student student;
+  final ValueChanged<bool> onChanged;
 
-  StudentCard({required this.student});
+  StudentCard({required this.student, required this.onChanged});
 
   @override
   _StudentCardState createState() => _StudentCardState();
 }
 
 class _StudentCardState extends State<StudentCard> {
-  bool isChecked = false;
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -65,11 +115,12 @@ class _StudentCardState extends State<StudentCard> {
         title: Text(widget.student.name),
         subtitle: Text('Roll No: ${widget.student.rollNumber}'),
         trailing: Checkbox(
-          value: isChecked,
+          value: widget.student.isPresent,
           onChanged: (bool? value) {
             setState(() {
-              isChecked = value ?? false;
+              widget.student.isPresent = value ?? false;
             });
+            widget.onChanged(widget.student.isPresent);
           },
         ),
       ),
@@ -77,9 +128,4 @@ class _StudentCardState extends State<StudentCard> {
   }
 }
 
-class Student {
-  final String name;
-  final String rollNumber;
 
-  Student({required this.name, required this.rollNumber});
-}
